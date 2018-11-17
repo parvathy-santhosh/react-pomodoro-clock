134c40520e8aac00c15d2a603c3be1976075c3fc
Parvathy Santhosh <parsan1998@gmail.com>
2018-11-17 14:31:54 +0530

    Updates

---
+++ precache-manifest.79edc5dd1a797b564f23069b867bf1d5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3369b338255bd04147c0",
+    "url": "/react-pomodoro-clock/static/js/runtime~main.3369b338.js"
+  },
+  {
+    "revision": "98cf71080e28710524a0",
+    "url": "/react-pomodoro-clock/static/js/main.98cf7108.chunk.js"
+  },
+  {
+    "revision": "0b13b4885e8ccff8ae99",
+    "url": "/react-pomodoro-clock/static/js/1.0b13b488.chunk.js"
+  },
+  {
+    "revision": "98cf71080e28710524a0",
+    "url": "/react-pomodoro-clock/static/css/main.2b487372.chunk.css"
+  },
+  {
+    "revision": "055fc3cdd9e77651822a4830534a2cb9",
+    "url": "/react-pomodoro-clock/index.html"
+  }
+];
